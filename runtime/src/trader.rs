e2b5670f557bf1b86db0d8463bd914fbc5d38155
N1ghtStorm <bushuevdv@mail.ru>
2022-09-09 12:43:10 +0200

    add trader implementation

--- runtime/src/trader.rs
+++ runtime/src/trader.rs
@@ -1,40 +1,60 @@
-use xcm_executor::{
-	traits::{DropAssets, WeightTrader},
-	Assets,
-};
-use xcm_builder::TakeRevenue;
 use frame_support::{
 	traits::Get,
 	weights::{constants::WEIGHT_PER_SECOND, Weight},
 };
 use sp_std::{marker::PhantomData, prelude::*};
 use xcm::latest::prelude::*;
-
+use xcm_builder::TakeRevenue;
+use xcm_executor::{
+	traits::{DropAssets, WeightTrader},
+	Assets,
+};
 
 pub struct ParachainTrader {
-    pub weight: Weight
+	pub weight: Weight,
+	multi_location: Option<MultiLocation>,
 }
 
 impl WeightTrader for ParachainTrader {
-    fn new() -> Self {
-		Self {
-			weight: 0,
-		}
+	fn new() -> Self {
+		log::trace!(target: "xcm::weight", "creating new WeightTrader instance");
+		Self { weight: 0, multi_location: None }
 	}
 
-    fn buy_weight(&mut self, weight: Weight, payment: Assets) -> Result<Assets, XcmError> {
+	fn buy_weight(&mut self, weight: Weight, payment: Assets) -> Result<Assets, XcmError> {
 		log::trace!(target: "xcm::weight", "buy_weight weight: {:?}, payment: {:?}", weight, payment);
+		let asset_id = payment
+			.fungible
+			.iter()
+			.next()
+			.map_or(Err(XcmError::TooExpensive), |v| Ok(v.0))?;
+            
+		let required = MultiAsset { id: asset_id.clone(), fun: Fungible(weight as u128) };
 
+		if let MultiAsset { fun: _, id: Concrete(ref id) } = &required {
+			self.multi_location = Some(id.clone());
+		} else {
 
-		log::trace!(target: "xcm::weight", "no concrete fungible asset");
-		Err(XcmError::TooExpensive)
+        }
+
+		let unused = payment.checked_sub(required).map_err(|_| XcmError::TooExpensive)?;
+		Ok(unused)
 	}
 
-    fn refund_weight(&mut self, weight: Weight) -> Option<MultiAsset> {
+	fn refund_weight(&mut self, weight: Weight) -> Option<MultiAsset> {
 		log::trace!(
 			target: "xcm::weight", "refund_weight weight: {:?} ",
 			weight
 		);
-        None
+		match &self.multi_location {
+			None => None,
+			Some(ml) => {
+                if weight == 0 {
+                    None
+                } else {
+                    Some((ml.clone(), weight as u128).into())
+                }
+            }
+		}
 	}
 }
