1c9aa9a0634c1480eaba1b5d9f54992e2a69048e
N1ghtStorm <bushuevdv@mail.ru>
2022-09-26 17:58:15 +0200

    add all func signatures

--- pallets/sora2-trustless-bridge/beefy-light-client/src/lib.rs
+++ pallets/sora2-trustless-bridge/beefy-light-client/src/lib.rs
@@ -16,36 +16,37 @@ pub use pallet::*;
 // mod benchmarking;
 
 pub struct Commitment {
-	payload_prefix: Vec<u8>,
-	payload: [u8; 32],
-	payload_suffix: Vec<u8>,
-	block_number: u32,
-	validator_set_id: u64,
+	pub payload_prefix: Vec<u8>,
+	pub payload: [u8; 32],
+	pub payload_suffix: Vec<u8>,
+	pub block_number: u32,
+	pub validator_set_id: u64,
 }
 
 pub struct ValidatorProof<AccountId> {
-	validator_claims_bitfield: Vec<u128>,
-	signatures: Vec<Vec<u8>>,
-	positions: Vec<u128>,
-	public_keys: AccountId,
-	public_key_merkle_proofs: Vec<Vec<[u8; 32]>>,
+	pub validator_claims_bitfield: Vec<u128>,
+	pub signatures: Vec<Vec<u8>>,
+	pub positions: Vec<u128>,
+	pub public_keys: AccountId,
+	pub public_key_merkle_proofs: Vec<Vec<[u8; 32]>>,
 }
 
 pub struct BeefyMMRLeaf {
-	version: u8,
-	parent_number: u32,
-	next_authority_set_id: u64,
-	next_authority_set_len: u32,
-	parent_hash: [u8; 32],
-	next_authority_set_root: [u8; 32],
-	random_seed: [u8; 32],
-	digest_hash: [u8; 32],
+	pub version: u8,
+	pub parent_number: u32,
+	pub next_authority_set_id: u64,
+	pub next_authority_set_len: u32,
+	pub parent_hash: [u8; 32],
+	pub next_authority_set_root: [u8; 32],
+	pub random_seed: [u8; 32],
+	pub digest_hash: [u8; 32],
 }
 
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
+	use super::*;
 
 	pub const MMR_ROOT_HISTORY_SIZE: u32 = 30;
 
@@ -57,9 +58,6 @@ pub mod pallet {
 	pub const MAXIMUM_BLOCK_GAP: u64 = NUMBER_OF_BLOCKS_PER_SESSION - ERROR_AND_SAFETY_BUFFER;
 
 	pub const MMR_ROOT_ID: [u8; 2] = [0x6d, 0x68];
-
-
-
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -99,7 +97,7 @@ pub mod pallet {
 	pub type NumOfValidators<T> = StorageValue<_, u128>;
 
 	#[pallet::storage]
-	#[pallet::getter(fn latest_mmr_root_index)]
+	#[pallet::getter(fn validator_registry_id)]
 	pub type ValidatorRegistryId<T> = StorageValue<_, u64>;
 
 	// Pallets use events to inform users when important changes are made.
@@ -205,20 +203,20 @@ pub mod pallet {
 			todo!()
 		}
 
-		pub fn verify_commitment(commitment: Commitment, proof: ValidatorProof) {
+		pub fn verify_commitment(commitment: Commitment, proof: ValidatorProof<T::AccountId>) {
 			todo!()
 		}
 
 		pub fn verify_validator_proof_lengths(
 			required_num_of_signatures: u128,
-			proof: ValidatorProof,
+			proof: ValidatorProof<T::AccountId>,
 		) {
 			todo!()
 		}
 
 		pub fn verify_validator_proof_signatures(
 			random_bitfield: [u8; 32],
-			proof: ValidatorProof,
+			proof: ValidatorProof<T::AccountId>,
 			required_num_of_signatures: u128,
 			commitment_hash: [u8; 32],
 		) {
@@ -248,7 +246,11 @@ pub mod pallet {
 			todo!()
 		}
 
-		pub fn validator_registry_update(new_root: [u8; 32], new_num_of_validators: u128, new_id: u64){
+		pub fn validator_registry_update(
+			new_root: [u8; 32],
+			new_num_of_validators: u128,
+			new_id: u64,
+		) {
 			todo!()
 		}
 
