a42045ddc543fa18585eae0b0780afb735738fc0
Dmitry <bushuevdv@mail.ru>
2023-06-05 16:58:15 +0200

    change spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -217,10 +217,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora_ksm"),
     impl_name: create_runtime_str!("sora_ksm"),
     authoring_version: 1,
-    spec_version: 2,
+    spec_version: 3,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 2,
+    transaction_version: 3,
     state_version: 1,
 };
 
