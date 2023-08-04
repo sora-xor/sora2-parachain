e73bfc23cf1bf38ee360e38bff10454d61715157
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-04 13:53:22 +0300

    test new workflow
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/scripts/build.sh
+++ housekeeping/scripts/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 
-parachains=(kusama rococo polkadot)
+networks=(kusama rococo polkadot)
 
 rm -rf ~/.cargo/registry/
 
-for parachain in ${parachains[@]}
+for network in ${networks[@]}
 do
- printf "🏗️ Building $parachain will starting now... %s/n"
- cargo b -r "$parachain"
- cargo test -r "$parachain"
- wasm_out=./sora2-parachain-runtime_$parachain.compact.wasm
- mv ./target/release/wbuild/sora2-parachain-runtime_$parachain.compact.wasm $wasm_out
+ printf "🏗️ Building " $network "will starting now... %s/n"
+ cargo b -r "$network"
+ cargo test -r "$network"
+ wasm_out=./sora2-parachain-runtime_$network.compact.wasm
+ mv ./target/release/wbuild/sora2-parachain-runtime_$network.compact.wasm $wasm_out
  if [ -f "$wasm_out" ]; then
-    printf "✅ $wasm_out OK %/n"
+    printf "✅" $wasm_out "OK %/n""
  else
     exit 1
  fi
