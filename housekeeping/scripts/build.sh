6cc6e86eb689a10940c45885d1a107e896adbcba
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-04 14:06:01 +0300

    fix workflow
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/scripts/build.sh
+++ housekeeping/scripts/build.sh
@@ -7,13 +7,13 @@ rm -rf ~/.cargo/registry/
 
 for network in ${networks[@]}
 do
- printf "🏗️ Building $network will starting now... /n"
+ printf "🏗️ Building "$network" will starting now... /n"
  cargo b -r "$network"
  cargo test -r "$network"
  wasm_out=./sora2-parachain-runtime_$network.compact.wasm
  mv ./target/release/wbuild/sora2-parachain-runtime_$network.compact.wasm $wasm_out
  if [ -f "$wasm_out" ]; then
-    printf "✅ $wasm_out OK\n"
+    printf "✅ "$wasm_out" OK\n"
  else
     exit 1
  fi
