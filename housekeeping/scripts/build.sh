46c81fb52784ca1ab1c4ea89d2dfa95bf2630cea
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-04 13:43:22 +0300

    test new workflow
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/scripts/build.sh
+++ housekeeping/scripts/build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+set -e
+
+parachains=(kusama rococo polkadot)
+
 rm -rf ~/.cargo/registry/
-cargo test -r
-cargo build --release
-cp target/release/parachain-collator housekeeping/parachain-collator
-mv ./target/release/wbuild/parachain-template-runtime/parachain_template_runtime.compact.compressed.wasm ./parachain_template_runtime.compact.compressed.wasm
+
+for parachain in ${parachains[@]}
+do
+ printf "🏗️ Building $parachain will starting now... %s/n"
+ cargo b -r "$parachain"
+ cargo test -r "$parachain"
+ wasm_out=./sora2-parachain-runtime_$parachain.compact.wasm
+ mv ./target/release/wbuild/sora2-parachain-runtime_$parachain.compact.wasm $wasm_out
+ if [ -f "$wasm_out" ]; then
+    printf "✅ $wasm_out OK %/n"
+ else
+    exit 1
+ fi
+done
