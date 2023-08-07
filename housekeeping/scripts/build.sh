e48363f806ae1a060e857aa50dbaee2f6bdf339b
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-07 10:21:58 +0300

    fix
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/scripts/build.sh
+++ housekeeping/scripts/build.sh
@@ -4,7 +4,6 @@ set -e
 buidcmd="cargo b -r --features"
 testcmd="cargo test -r --features"
 networks=(kusama rococo polkadot)
-wasm_in="./target/release/wbuild/sora2-parachain-runtime_"
 
 rm -rf ~/.cargo/registry/
 
@@ -13,11 +12,13 @@ do
  printf "🏗️ Build "$network" will start now... \n"
  $buidcmd "$network"
  $testcmd "$network"
- wasm_out=./sora2-parachain-runtime_$network.compact.wasm
- mv "$wasm_in$network.compact.wasm" "$wasm_out"
+ wasm_in="./target/release/wbuild/$network-runtime/"
+ wasm_out=./sora2-parachain-runtime_$network.compressed.wasm
+ wasm_file=$(ls "$wasm_in" | grep "$network" | grep ".compressed.wasm")
+ mv "$wasm_in$wasm_file" "$wasm_out"
  if [ -f "$wasm_out" ]; then
     printf "✅ "$wasm_out" OK\n"
  else
-    exit 1
+    exit 0
  fi
 done
