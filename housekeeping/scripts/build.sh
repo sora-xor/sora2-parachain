1ee68fffcfc2560489d59aea6bd28961bbadc43f
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-07 15:18:49 +0300

    compact
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/scripts/build.sh
+++ housekeeping/scripts/build.sh
@@ -9,16 +9,17 @@ rm -rf ~/.cargo/registry/
 
 for network in ${networks[@]}
 do
- printf "🏗️ Build "$network" will start now... \n"
+ printf "🏗️ Building of "$network" will start now... \n"
  $buidcmd "$network"
  $testcmd "$network"
- wasm_in="./target/release/wbuild/$network-runtime/"
- wasm_out=./sora2-parachain-runtime_$network.compressed.wasm
- wasm_file=$(ls "$wasm_in" | grep "$network" | grep ".compressed.wasm")
+ wasm_in="./target/release/wbuild/sora2-parachain-runtime/"
+ wasm_out=./sora2-parachain-runtime_$network.compact.compressed.wasm
+ wasm_file=$(ls "$wasm_in" | grep ".compact.compressed.wasm")
  mv "$wasm_in$wasm_file" "$wasm_out"
  if [ -f "$wasm_out" ]; then
-    printf "✅ "$wasm_out" OK\n"
+    printf "✅ "$wasm_out" found!\n"
  else
+    printf "❌"$wasm_out" can't found!\n"
     exit 1
  fi
 done
