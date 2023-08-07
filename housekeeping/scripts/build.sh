599fbd1c2462591a32addf30ae027e93066ba500
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-07 17:13:31 +0300

    update workflow
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/scripts/build.sh
+++ housekeeping/scripts/build.sh
@@ -7,19 +7,30 @@ networks=(kusama rococo polkadot)
 
 rm -rf ~/.cargo/registry/
 
+
 for network in ${networks[@]}
 do
- printf "🏗️ Building of "$network" will start now... \n"
- $buidcmd "$network"
- $testcmd "$network"
- wasm_in="./target/release/wbuild/sora2-parachain-runtime/"
- wasm_out=./sora2-parachain-runtime_$network.compact.compressed.wasm
- wasm_file=$(ls "$wasm_in" | grep ".compact.compressed.wasm")
- mv "$wasm_in$wasm_file" "$wasm_out"
- if [ -f "$wasm_out" ]; then
-    printf "✅ "$wasm_out" found!\n"
- else
-    printf "❌"$wasm_out" can't found!\n"
-    exit 1
- fi
+   if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
+      printf "🏗️ Building of "$network" will start now... \n"
+      $buidcmd "$network"
+      $testcmd "$network"
+      wasm_in="./target/release/wbuild/sora2-parachain-runtime/"
+      wasm_out=./sora2-parachain-runtime_$network.compact.compressed.wasm
+      wasm_file=$(ls "$wasm_in" | grep ".compact.compressed.wasm")
+      mv "$wasm_in$wasm_file" "$wasm_out"
+   else
+      printf "⚡️ There is no tag here, only tests run. \n"
+      printf "🏃 Running tests for "$network"... \n"
+      $testcmd "$network"
+      wasm_in="./target/release/wbuild/sora2-parachain-runtime/"
+      wasm_out=./sora2-parachain-runtime_$network.compact.compressed.wasm     
+      wasm_file=$(ls "$wasm_in" | grep ".compact.compressed.wasm")
+      mv "$wasm_in$wasm_file" "$wasm_out"
+   fi
+   if [ -f "$wasm_out" ]; then
+      printf "✅ "$wasm_out" found!\n"
+   else
+      printf "❌"$wasm_out" can't found!\n"
+      exit 1
+   fi
 done
