09efe7e1b408db76765cbd8e214ce19a6dcd53a9
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-09-06 17:49:09 +0300

    update
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,4 @@
-@Library('jenkins-library') _
+@Library('jenkins-library@duty/parachain-bench') _
 
 def pipeline = new org.rust.AppPipeline(steps: this,
       envImageName: 'docker.soramitsu.co.jp/sora2/parachain-env:latest',
--- misc/run_all_pallet_benches.local.sh
+++ misc/run_all_pallet_benches.local.sh
@@ -3,8 +3,11 @@
 # Runs all benchmarks for all pallets, for a given runtime, provided by $1
 # Should be run on a reference machine to gain accurate benchmarks
 # current reference machine: https://github.com/paritytech/substrate/pull/5848
-# Load all pallet names in an array.
 
+echo "[+] Compiling benchmarks..."
+cargo build --release --locked --features runtime-benchmarks,rococo --bin framenode
+
+# Load all pallet names in an array.
 PALLETS=($(
   ./target/release/parachain-collator benchmark pallet --list --chain="local" |\
     tail -n+2 |\
@@ -91,5 +94,3 @@ if [ -f "$ERR_FILE" ]; then
 else
   echo "[+] All benchmarks passed."
 fi
-
-cat benchmarking_errors.txt
