c95b6e0ec793d1c08933b09d8261345f438d57a5
f33r0 <95526886+f33r0@users.noreply.github.com>
2023-09-22 18:20:55 +0300

    Update build.sh

--- housekeeping/scripts/build.sh
+++ housekeeping/scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 benchmarkcmd="cargo build --release --locked --features runtime-benchmarks,kusama --bin parachain-collator"
 buidcmd="cargo b -r --features"
 testcmd="cargo test -r --features"
-becnchfeature="rococo runtime-benchmarks"
+becnchfeature="runtime-benchmarks"
 networks=(kusama rococo polkadot)
 binaryfile="target/release/parachain-collator"
 binaryfilepath="housekeeping/parachain-collator"
@@ -30,8 +30,7 @@ do
    else
       printf "⚡️ There is no tag here, only tests run. \n"
       printf "🏃 Running tests for "$network"... \n"
-      $testcmd "$network"
-      $testcmd "$becnchfeature"
+      $testcmd "$network" "$becnchfeature"
       wasm_in="./target/release/wbuild/sora2-parachain-runtime/"
       wasm_out=./sora2-parachain-runtime_$network.compact.compressed.wasm     
       wasm_file=$(ls "$wasm_in" | grep ".compact.compressed.wasm")
