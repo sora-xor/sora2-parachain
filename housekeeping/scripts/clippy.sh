8684913c5b88f65c75ba56640ed5c53bb85c4038
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-09-28 09:26:17 +0300

    update clippy script
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/scripts/clippy.sh
+++ housekeeping/scripts/clippy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+clippycommand="SKIP_WASM_BUILD=1 cargo clippy --features"
+clippyfeatures=("kusama" "polkadot" "rococo,runtime-benchmarks")
+
 if [ "$pr" = true ] && [ "$prBranch" != "master" ]; then
-    printf "👷‍♂️ starting clippy \n"
-    SKIP_WASM_BUILD=1 cargo clippy --features kusama
-    SKIP_WASM_BUILD=1 cargo clippy --features polkadot
-    SKIP_WASM_BUILD=1 cargo clippy --features rococo,runtime-benchmarks
+    for clippyfeature in "${clippyfeatures[@]}"; do
+        printf "👷‍♂️ starting clippy with $clippyfeature feature \n"
+        $clippycommand $clippyfeature
+    done
 else
     printf "👷‍♂️ starting a regular clippy \n"
     cargo clippy -- -D warnings || exit 0
