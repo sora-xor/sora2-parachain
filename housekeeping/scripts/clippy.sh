1f84f507263d3ba88d8423ae29570c9f5fea509c
C4tWithShell <shelkovnikov@soramitsu.co.jp>
2023-11-22 12:25:20 +0300

    Configure clippy, coverage reports

--- housekeeping/scripts/clippy.sh
+++ housekeeping/scripts/clippy.sh
@@ -3,9 +3,9 @@ set -e
 
 if [ "$pr" = true ] && [ "$prBranch" != "master" ]; then
     printf "👷‍♂️ starting clippy \n"
-    SKIP_WASM_BUILD=1 cargo clippy --features kusama
-    SKIP_WASM_BUILD=1 cargo clippy --features polkadot
-    SKIP_WASM_BUILD=1 cargo clippy --features rococo,runtime-benchmarks
+    SKIP_WASM_BUILD=1 cargo clippy --features kusama --message-format=json > clippy_kusama_report.json
+    SKIP_WASM_BUILD=1 cargo clippy --features polkadot --message-format=json > clippy_polkadot_report.json
+    SKIP_WASM_BUILD=1 cargo clippy --features rococo,runtime-benchmarks --message-format=json > clippy_rococo_report.json
 else
     printf "👷‍♂️ starting a regular clippy \n"
     cargo clippy --message-format=json -- -D warnings > clippy_report.json || exit 0
