2f1e2055fa6573b4be39db12acb61db8220eb475
Vladimir Stepanenko <vovac12@gmail.com>
2024-01-16 19:10:25 +0300

    Enable sudo on polkadot parachain (#196)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -497,7 +497,7 @@ impl pallet_beefy_mmr::Config for Runtime {
     type BeefyDataProvider = LeafProvider;
 }
 
-#[cfg(feature = "rococo")]
+#[cfg(any(feature = "rococo", feature = "polkadot"))]
 impl pallet_sudo::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type RuntimeCall = RuntimeCall;
@@ -1059,7 +1059,7 @@ construct_runtime!(
         // ORML
         XTokens: orml_xtokens::{Pallet, Storage, Event<T>} = 41,
 
-        #[cfg(feature = "rococo")]
+        #[cfg(any(feature = "rococo", feature = "polkadot"))]
         Sudo: pallet_sudo::{Pallet, Call, Storage, Event<T>, Config<T>} = 100,
 
         XCMApp: xcm_app::{Pallet, Call, Storage, Event<T>} = 101,
