df923a54045f617c6253fc18feb17ccc0f2488bd
Dmitry <bushuevdv@mail.ru>
2024-01-19 11:04:47 -0300

    dependencies

--- node/src/command.rs
+++ node/src/command.rs
@@ -78,7 +78,7 @@ impl SubstrateCli for Cli {
     }
 
     fn support_url() -> String {
-        "https://github.com/paritytech/cumulus/issues/new".into()
+        "https://github.com/paritytech/polkadot-sdk.git-sdk.git/issues/new".into()
     }
 
     fn copyright_start_year() -> i32 {
@@ -118,7 +118,7 @@ impl SubstrateCli for RelayChainCli {
     }
 
     fn support_url() -> String {
-        "https://github.com/paritytech/cumulus/issues/new".into()
+        "https://github.com/paritytech/polkadot-sdk.git-sdk.git/issues/new".into()
     }
 
     fn copyright_start_year() -> i32 {
