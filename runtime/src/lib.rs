bd159b9ea48e967333ff0074b5b4a4add247bcaf
Dmitry <bushuevdv@mail.ru>
2024-03-06 14:43:13 -0300

    change trader

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -221,10 +221,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora_ksm"),
     impl_name: create_runtime_str!("sora_ksm"),
     authoring_version: 1,
-    spec_version: 12,
+    spec_version: 13,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 12,
+    transaction_version: 13,
     state_version: 1,
 };
 
