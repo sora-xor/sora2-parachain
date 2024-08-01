881a18ba42618eb26fd2b20b3377fa215a18492d
Nikita Zaporozhets <nan3251234@gmail.com>
2024-08-01 17:08:31 +0300

    test
    Signed-off-by: Nikita Zaporozhets <nan3251234@gmail.com>

--- housekeeping/scripts/coverage.sh
+++ housekeeping/scripts/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-grcov . --binary-path ./target/release -s . -t lcov --branch -o ./lcov_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/release" "node/src" --log-level="ERROR" --llvm-path='/usr/lib/llvm-14/bin'
+grcov . --binary-path ./target/release -s . -t lcov --branch -o ./lcov_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/release" "node/src" --log-level="ERROR" --llvm-path='/usr/lib/llvm-15/bin'
 
 find . -type f -name '*.profraw' -delete
